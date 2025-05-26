25930f33541d24a01d46394c38e0e2ce30a10f8f
Garrett Smith <garrettsmith78@gmail.com>
2025-05-26 10:46:13 +0200

    Add first draft of meta-analysis endpoint

--- Program.fs
+++ Program.fs
@@ -8,14 +8,14 @@ type DataPoint =
     { Id: Guid
       ExpName: string
       EffectSize: float
-      StdError: float option }
+      StdError: float }
 
 type DataPointCollection = { mutable Items: DataPoint array }
 
 type RawData =
     { ExpName: string
       EffectSize: float
-      StdError: float option }
+      StdError: float }
 
 let addItem (collection: DataPointCollection) (rawData: RawData) =
     let guid = Guid.NewGuid()
@@ -41,6 +41,27 @@ let getHandler (collection: DataPointCollection) : HttpHandler =
     fun ctx -> task { return Response.ofPlainText (collection.ToString()) ctx }
 
 
+type MetaAnalyticEstimate = { Mean: float }
+
+let doMetaAnalysis (datapoints: DataPointCollection) : MetaAnalyticEstimate =
+    let effectsizes = datapoints.Items |> Array.map (fun x -> x.EffectSize)
+
+    let weights =
+        datapoints.Items
+        |> Array.map (fun datapoint -> 1.0 / (datapoint.StdError ** 2.0))
+
+    let weightedSum =
+        Array.zip effectsizes weights |> Array.map (fun (m, w) -> m * w) |> Array.sum
+
+    { Mean = weightedSum / (Array.sum weights) }
+
+let resultHandler (collection: DataPointCollection) : HttpHandler =
+    fun ctx ->
+        task {
+            let res = doMetaAnalysis collection
+            return Response.ofPlainText $"The meta-analytic estimate is: {res.Mean}" ctx
+        }
+
 [<EntryPoint>]
 let main args =
     let mutable datapoints =
@@ -48,10 +69,12 @@ let main args =
             [| { Id = Guid.NewGuid()
                  ExpName = "test"
                  EffectSize = 0.0
-                 StdError = Some 1.0 } |] }
+                 StdError = 1.0 } |] }
 
     let endpoints =
-        [ get "/" (getHandler datapoints); post "/create" (postHandler datapoints) ]
+        [ get "/" (getHandler datapoints)
+          post "/create" (postHandler datapoints)
+          get "/result" (resultHandler datapoints) ]
 
     let wapp = WebApplication.Create()
 
