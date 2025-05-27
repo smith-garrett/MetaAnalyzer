9767c0f02343ed9573b129e8d5767500c0508d4e
Garrett Smith <garrettsmith78@gmail.com>
2025-05-27 13:53:21 +0200

    Add additional test, add Program.fs to test project to get rid of warning.

--- MetaAnalyzer.Tests/Tests.fs
+++ MetaAnalyzer.Tests/Tests.fs
@@ -5,8 +5,8 @@ open Xunit
 
 open MetaAnalyzer.MetaAnalyzer
 
-[<Fact>]
-let ``My test`` () = Assert.True(true)
+// [<Fact>]
+// let ``My test`` () = Assert.True(true)
 
 
 [<Fact>]
@@ -20,3 +20,20 @@ let ``The meta-analytic result of a single effect size should equal the effect s
 
     let est = doMetaAnalysis datapoints
     Assert.Equal(datapoints.DataPoints[0].EffectSize, est.Mean)
+
+[<Fact>]
+let ``The meta-analytic estimate of 10 and 12 with equal std. errors should be 11`` () =
+    let datapoints = DataPointCollection.Default
+
+    datapoints.addItem
+        { ExpName = "test1"
+          EffectSize = 10.0
+          StdError = 1.0 }
+
+    datapoints.addItem
+        { ExpName = "test2"
+          EffectSize = 12.0
+          StdError = 1.0 }
+
+    let est = doMetaAnalysis datapoints
+    Assert.Equal(11.0, est.Mean)
