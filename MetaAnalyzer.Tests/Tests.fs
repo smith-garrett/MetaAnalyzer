c0fa0401869d9244239eeaa1d04859224a4a196b
Garrett Smith <garrettsmith78@gmail.com>
2025-05-27 13:42:30 +0200

    Restructure and add first test

---
+++ MetaAnalyzer.Tests/Tests.fs
@@ -0,0 +1,22 @@
+﻿module Tests
+
+open System
+open Xunit
+
+open MetaAnalyzer.MetaAnalyzer
+
+[<Fact>]
+let ``My test`` () = Assert.True(true)
+
+
+[<Fact>]
+let ``The meta-analytic result of a single effect size should equal the effect size`` () =
+    let datapoints = DataPointCollection.Default
+
+    datapoints.addItem
+        { ExpName = "test"
+          EffectSize = 10.0
+          StdError = 1.0 }
+
+    let est = doMetaAnalysis datapoints
+    Assert.Equal(datapoints.DataPoints[0].EffectSize, est.Mean)
--- MetaAnalyzer/Program.fs
+++ MetaAnalyzer/Program.fs
@@ -2,66 +2,8 @@ open System
 open Falco
 open Falco.Routing
 open Microsoft.AspNetCore.Builder
+open MetaAnalyzer.MetaAnalyzer
 
-
-type DataPoint =
-    { Id: Guid
-      ExpName: string
-      EffectSize: float
-      StdError: float }
-
-type RawData =
-    { ExpName: string
-      EffectSize: float
-      StdError: float }
-
-type DataPointCollection =
-    { mutable DataPoints: DataPoint list }
-
-    static member Default = { DataPoints = List.empty<DataPoint> }
-
-    member this.addItem(rawData: RawData) =
-        let guid = Guid.NewGuid()
-
-        let newItem =
-            { Id = guid
-              ExpName = rawData.ExpName
-              EffectSize = rawData.EffectSize
-              StdError = rawData.StdError }
-
-        this.DataPoints <- List.append this.DataPoints [ newItem ]
-
-type MetaAnalyticEstimate = { Mean: float }
-
-let postHandler (datapoints: DataPointCollection) : HttpHandler =
-    fun ctx ->
-        task {
-            let! newData = Request.getJson<RawData> ctx
-            datapoints.addItem newData
-            return Response.ofJson datapoints ctx
-        }
-
-let getHandler datapoints : HttpHandler =
-    fun ctx -> task { return Response.ofPlainText (datapoints.ToString()) ctx }
-
-let doMetaAnalysis (datapoints: DataPointCollection) : MetaAnalyticEstimate =
-    let effectsizes = datapoints.DataPoints |> List.map (fun x -> x.EffectSize)
-
-    let weights =
-        datapoints.DataPoints
-        |> List.map (fun datapoint -> 1.0 / datapoint.StdError ** 2.0)
-
-    let weightedSum =
-        List.zip effectsizes weights |> List.map (fun (m, w) -> m * w) |> List.sum
-
-    { Mean = weightedSum / List.sum weights }
-
-let resultHandler (datapoints: DataPointCollection) : HttpHandler =
-    fun ctx ->
-        task {
-            let res = doMetaAnalysis datapoints
-            return Response.ofPlainText $"The meta-analytic estimate is: {res.Mean}" ctx
-        }
 
 [<EntryPoint>]
 let main args =
